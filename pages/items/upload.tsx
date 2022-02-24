2b5dcd0615982d5b64c11a160a17e40986c63c57
Lee yongjun <chuck5732590@gmail.com>
2022-02-24 21:37:36 +0900

    feat(community): 커뮤니티 디테일 페이지 추가

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
   return (
-    <div className="px-4 py-16">
+    <div className="px-4 py-10">
       <div>
         <label
           htmlFor="upload"
