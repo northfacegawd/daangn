5be11ad12d10c0126359dd4ed931bfaa718d78a0
Lee yongjun <chuck5732590@gmail.com>
2022-04-06 20:25:51 +0900

    feat(nodemailer): nodemailer 라이브러리를 사용하여 메일로 토큰 발송

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,9 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import twilio from "twilio";
+import nodemailer from "nodemailer";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+const transport = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.ADMIN_EMAIL,
+    pass: process.env.ADMIN_EMAIL_PASSWORD,
+  },
+});
 
 async function handler(
   req: NextApiRequest,
@@ -34,7 +45,17 @@ async function handler(
       body: `Your login token is ${payload}`,
     });
     console.log(message);
+  } else if (email) {
+    const sendEmail = await transport.sendMail({
+      from: "carrotmarket.services@gmail.com",
+      to: email,
+      subject: "Your Carrot Market Verification Email",
+      text: `Your login token is ${payload}`,
+      html: `<strong>Your login token is ${payload}</strong>`,
+    });
+    console.log(sendEmail);
   }
+
   return res.status(200).json({ ok: true });
 }
 
