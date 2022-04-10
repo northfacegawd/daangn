d7b6318d0fea8182bafadf9996a3988c366937a2
Lee yongjun <chuck5732590@gmail.com>
2022-04-10 12:17:15 +0900

    feat(posts): post detail api 추가 및 프론트 대응

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,56 @@
+import withHandler from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    query: { id },
+  } = req;
+  try {
+    const post = await client.post.findUnique({
+      where: {
+        id: +id.toString(),
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          },
+        },
+        answers: {
+          select: {
+            answer: true,
+            id: true,
+            createdAt: true,
+            updatedAt: true,
+            user: {
+              select: {
+                id: true,
+                name: true,
+                avatar: true,
+              },
+            },
+          },
+        },
+        _count: {
+          select: {
+            answers: true,
+            wonderings: true,
+          },
+        },
+      },
+    });
+    if (!post) {
+      return res.status(404).json({ ok: false, error: "NOT_FOUND_POST" });
+    }
+    return res.status(200).json({ ok: true, post });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ ok: false, error });
+  }
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
