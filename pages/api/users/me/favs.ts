9bd279d035fced1420672b17cc704cae52b0959a
Lee yongjun <chuck5732590@gmail.com>
2022-04-10 19:00:29 +0900

    feat(profile): 구매목록, 판매목록, 관심목록 api추가 (TODO - 해당 3가지 모델을 하나로 묶는 리팩토링 진행해야함)

---
+++ pages/api/users/me/favs.ts
@@ -0,0 +1,24 @@
+import withHandler from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    session: { user },
+  } = req;
+  const favs = await client.fav.findMany({
+    where: {
+      userId: user?.id,
+    },
+    include: {
+      product: true,
+    },
+  });
+  return res.status(200).json({
+    ok: true,
+    favs,
+  });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
